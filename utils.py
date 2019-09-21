b3a84627271be301dd42cbab93d5a8919b5d0571
Ronald Theodoro <ronaldtheodoro@gmail.com>
2019-09-21 18:34:13 -0300

    Add is_url funcion

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@
 
 import os
 import re
-import urllib
+import urllib.parse
 
 import lxml.html
 import requests
@@ -31,3 +31,19 @@ def create_html_element_instance(response, base_url):
     root = lxml.html.fromstring(response.text, base_url=base_url)
     root.make_links_absolute()
     return root
+
+
+def is_url(url):
+    """Checks if it is a valid URL
+
+    Args:
+        url (str): URL string.
+
+    Returns:
+        bool: Returns true if it is a valid URL.
+    """
+    try:
+        result = urllib.parse.urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
