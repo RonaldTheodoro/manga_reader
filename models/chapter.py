c2e4c4673f3a67dc236b63af0941125f46ffed59
Ronald Theodoro <ronaldtheodoro@gmail.com>
2019-09-21 02:10:25 -0300

    Implement Chapter properties

--- models/chapter.py
+++ models/chapter.py
@@ -3,6 +3,28 @@
 
 
 class Chapter(object):
+    _link = None
     _number = None
     _title = None
     _pages = []
+
+    def __init__(self, link, number, title):
+        self._link = link
+        self._number = number
+        self._title = title
+
+    @property
+    def link(self):
+        return self._link
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def pages(self):
+        return self._pages
